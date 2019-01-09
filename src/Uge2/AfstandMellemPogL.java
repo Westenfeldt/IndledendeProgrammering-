c38f5b8a01adc8b1f40495dd1f988f4ba86625d5
Stefan Vestenfeldt <stefanwestenfeldt@gmail.com>
2019-01-09 20:01:27 +0100

    Opgave færdiggjort

--- src/Uge2/AfstandMellemPogL.java
+++ src/Uge2/AfstandMellemPogL.java
@@ -1,7 +1,23 @@
 package Uge2;
 
+import java.util.Scanner;
+
 public class AfstandMellemPogL {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+    System.out.println("Indtast værdierne for a og b");
+                double a = scan.nextDouble();
+                double b = scan.nextDouble();
+
+    System.out.println("Indtast koordinaterne for punktet P");
+                double x = scan.nextDouble();
+                double y = scan.nextDouble();
+
+    double afstand = (a * x - y + b)/Math.sqrt(1 + a*a);
+
+    System.out.println("Afstanden er: " + afstand);
 
     }
 }
+
